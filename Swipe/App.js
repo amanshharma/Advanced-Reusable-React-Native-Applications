713ba8b99dae79cd6ff7c5b180dff0592c503387
amanshharma <amandeep.upes@gmail.com>
2019-07-10 00:44:52 -0400

    Added the initial app structure

---
+++ Swipe/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aman ...Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
