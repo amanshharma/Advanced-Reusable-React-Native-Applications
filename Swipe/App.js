4649cb9086e9581960effa61362b3d46b640c310
amanshharma <amandeep.upes@gmail.com>
2019-07-22 23:25:08 -0400

    added render card

--- Swipe/App.js
+++ Swipe/App.js
@@ -1,11 +1,59 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Ball from "./components/Ball";
+import Deck from "./components/Deck";
+
+const DATA = [
+  {
+    id: 1,
+    text: "Card #1",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg"
+  },
+  {
+    id: 2,
+    text: "Card #2",
+    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg"
+  },
+  {
+    id: 3,
+    text: "Card #3",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg"
+  },
+  {
+    id: 4,
+    text: "Card #4",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg"
+  },
+  {
+    id: 5,
+    text: "Card #5",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg"
+  },
+  {
+    id: 6,
+    text: "Card #6",
+    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg"
+  },
+  {
+    id: 7,
+    text: "Card #7",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg"
+  },
+  {
+    id: 8,
+    text: "Card #8",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg"
+  }
+];
 
 export default function App() {
+
+  renderCard = () => {
+    return <Text>Carddd</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Ball />
+      <Deck data={DATA} renderCard={renderCard}/>
     </View>
   );
 }
---
+++ Swipe/components/Deck.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+const Deck = () => {
+  return (
+    <View>
+      <Text>Deck!</Text>
+    </View>
+  );
+};
+
+export default Deck;
