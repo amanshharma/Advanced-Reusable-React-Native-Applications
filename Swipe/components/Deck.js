86c88791f42fed49f08b93d9153ad3631f3bff91
Aman Sharma <aman@gamerlnk.gg>
2019-08-04 17:58:21 -0400

    folder structure changed and code reformatted

--- Swipe/components/Deck.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-
-const Deck = ({ renderCard, data }) => {
-  return <View>{data.map((item, index) => renderCard(item, index))}</View>;
-};
-
-export default Deck;
